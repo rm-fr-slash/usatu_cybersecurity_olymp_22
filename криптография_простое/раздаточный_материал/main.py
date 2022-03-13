938c819f4aee45a0dbda3ae7d175de2514712167
rm -fr / <101119916+rm-fr-slash@users.noreply.github.com>
2022-03-13 16:05:07 +0500

    Delete криптография_простое directory

--- криптография_простое/раздаточный_материал/main.py
+++
@@ -1,7 +0,0 @@
-new_msg = ''
-with open('послание.txt', 'r') as f:
-    msg = f.read()
-    for symb in msg:
-        new_msg += (str((ord(symb) + 13371337) * 13371337) + ' ')
-with open('послание.txt', 'w') as f:
-    f.write(new_msg)
